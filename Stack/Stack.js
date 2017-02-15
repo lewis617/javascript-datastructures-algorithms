4bb8c762e3911abeb6dab45ae1ebdd8476e88928
lewis617 <897473127@qq.com>
2017-02-15 12:28:54 +0800

    Update Stack

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,23 +1,31 @@
 function Stack() {
+  // 私有变量 items，用于记录数组，对象不能直接操作
   var items = [];
+  // 类方法 push，在数组末尾添加项，对象可以直接调用
   this.push = function (element) {
     items.push(element);
   };
+  // 删除并返回数组末尾的项
   this.pop = function () {
     return items.pop();
   };
+  // 查看数组最后一项
   this.peek = function () {
     return items[items.length - 1];
   };
+  // 判断数组是否为空
   this.isEmpty = function () {
     return items.length == 0;
   };
+  // 清空数组
   this.clear = function () {
     items = [];
   };
+  // 返回数组长度
   this.size = function () {
     return items.length;
   };
+  // 将数组转为字符串并返回
   this.toString = function () {
     return items.toString();
   };
--- Stack/Stack.test.js
+++ Stack/Stack.test.js
@@ -1,16 +1,28 @@
+// 导入 Stack
 var Stack = require('./Stack');
 
 test('Stack', function () {
+  // 实例化一个 stack 对象
   var stack = new Stack();
+
+  // 期望 stack 为空
   expect(stack.isEmpty()).toBeTruthy();
+
   stack.push(5);
   stack.push(8);
+  // 期望 stack 最后一项是8
   expect(stack.peek()).toBe(8);
+  // 期望 stack 的长度为2
   expect(stack.size()).toBe(2);
+  // 期望 stack 不为空
   expect(stack.isEmpty()).toBeFalsy();
+
   stack.pop();
+  // 期望 stack 长度为1
   expect(stack.size()).toBe(1);
+  // 期望 stack 转化成的字符串为'5'
   expect(stack.toString()).toBe('5');
   stack.clear();
+  // 期望 stack 长度为0
   expect(stack.size()).toBe(0);
 });
