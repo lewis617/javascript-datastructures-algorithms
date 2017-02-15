84088852532e64173beb6254c982488e736d80f3
lewis617 <897473127@qq.com>
2017-02-15 16:22:28 +0800

    Update Queue

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,20 +1,27 @@
 function Queue() {
+  // 私有变量 items，用于记录数组
   var items = [];
+  // 入队
   this.enqueue = function (element) {
     items.push(element);
   };
+  // 出队
   this.dequeue = function () {
     return items.shift();
   };
+  // 查看队列的第一个元素
   this.front = function () {
     return items[0];
   };
+  // 查看队列是否为空
   this.isEmpty = function () {
     return items.length == 0;
   };
+  // 查看队列的长度
   this.size = function () {
     return items.length;
   };
+  // 将数组转为字符串并返回
   this.toString = function () {
     return items.toString();
   };
